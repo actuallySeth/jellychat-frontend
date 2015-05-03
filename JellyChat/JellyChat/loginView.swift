64284c2f4143725e96da1d1c0fc767e81badf418
omarwqureshi <omarwqureshi@yahoo.com>
2015-05-02 19:16:15 -0700

    sumasdfj
    afsdfasdf

---
+++ JellyChat/JellyChat/loginView.swift
@@ -0,0 +1,31 @@
+
+//
+//  loginView.swift
+//  JellyChat
+//
+//  Created by Omar Qureshi on 5/2/15.
+//  Copyright (c) 2015 Arjun Sethi. All rights reserved.
+//
+
+
+import UIKit
+
+class loginView: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+    
+    
+    
+}
+
---
+++ JellyChat/JellyChat/signupVIew.swift
@@ -0,0 +1,9 @@
+//
+//  signupVIew.swift
+//  JellyChat
+//
+//  Created by Omar Qureshi on 5/2/15.
+//  Copyright (c) 2015 Arjun Sethi. All rights reserved.
+//
+
+import Foundation
