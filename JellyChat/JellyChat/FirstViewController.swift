bb6a73cf3c2673c0b6ed916a86023939f9695808
Arjun <arjunsethi9@gmail.com>
2015-05-02 17:47:32 -0700

    Omar work

---
+++ JellyChat/JellyChat/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  JellyChat
+//
+//  Created by Arjun Sethi on 5/2/15.
+//  Copyright (c) 2015 Arjun Sethi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- JellyChat/JellyChat/SecondViewController.swift
+++ JellyChat/JellyChat/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  JellyChat
 //
 //  Created by Arjun Sethi on 5/2/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
