611a8b25c774ecb8b7abf32222bed176a7416391
Julian Meyer <julianmeyer2000@gmail.com>
2015-05-02 22:47:42 -0700

    added some javascript

---
+++ src/app.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    $("#Start-Chat").click(function() {
+        if($(".chat-name").val() != "" && $(".chat-room").val() != "") {
+            nick = $(".chat-name").val()
+            room = $(".chat-room").val()
+            $(".chat-config").hide()
+            $(".chat").show()
+        }
+    }); 
+}
